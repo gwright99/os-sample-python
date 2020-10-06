293d87205f9cf81f181aa0b831e041357158b4d9
Graham Wright <gwright99@hotmail.com>
2020-10-06 10:50:24 -0400

    Updated wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,9 +1,16 @@
 from flask import Flask
 application = Flask(__name__)
 
+
 @application.route("/")
 def hello():
     return "Hello World!"
+
+
+@application.route("/newendpoint")
+def newendpoint():
+	return "This is a new endpoint I added on my local machine, pushed to Github, and then rebuilt via OpenShift S2i"
+
 
 if __name__ == "__main__":
     application.run()
