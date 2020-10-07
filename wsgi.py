c0bdc7ad0154eac67ab18eb42206234e367ecea5
Graham Wright <gwright99@hotmail.com>
2020-10-06 20:52:41 -0400

    Added '/get_dbs' endpoint to wsgi.py which calls mysql-python container

--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,6 @@
 from flask import Flask
+import mysql.connector
+
 application = Flask(__name__)
 
 
@@ -10,6 +12,15 @@ def hello():
 @application.route("/newendpoint")
 def newendpoint():
 	return "This is a new endpoint I added on my local machine, pushed to Github, and then rebuilt via OpenShift S2i"
+
+
+@application.route("/get_dbs")
+def get_dbs():
+	mydb = mysql.connector.connect(host="mysql-python", user="user1", password="mypa55")
+	c = mydb.cursor()
+	c.execute("show databases")
+	dbs = ','.join(str(x[0]) for x in c)
+	return f"Databases are: {dbs}."
 
 
 if __name__ == "__main__":
