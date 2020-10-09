9cae44fa098649c671098f00e0fe6f9add853d54
Graham Wright <gwright99@hotmail.com>
2020-10-09 09:23:18 -0400

    Using root for all db interactions (for ease of example)

--- wsgi.py
+++ wsgi.py
@@ -4,6 +4,13 @@ import mysql.connector
 application = Flask(__name__)
 
 
+def get_db_connection():
+	mydb = mysql.connector.connect(host="mysql-python", user="root", password="r00tpa55")
+	# mydb = mysql.connector.connect(host="mysql-python", user="user1", password="mypa55")
+	mycursor = mydb.cursor()
+	return mycursor
+
+
 @application.route("/")
 def hello():
     return "Hello World!"
@@ -16,26 +23,31 @@ def newendpoint():
 
 @application.route("/get_dbs")
 def get_dbs():
-	mydb = mysql.connector.connect(host="mysql-python", user="user1", password="mypa55")
-	c = mydb.cursor()
-	c.execute("show databases")
+	#mydb = mysql.connector.connect(host="mysql-python", user="user1", password="mypa55")
+	# mydb = get_db_connection()
+	# c = mydb.cursor()
+	cursor = get_db_connection()
+
+	
+	cursor.execute("show databases")
 	dbs = ','.join(str(x[0]) for x in c)
-	return f"Databases are: {dbs}."
+	return f"\nDatabases are: {dbs}.\n"
 
 
 @application.route("/create_db/<db_name>", methods=['GET'])
 def create_db(db_name):
-	mydb = mysql.connector.connect(host="mysql-python", user="root", password="r00tpa55")
-	c = mydb.cursor()
+	# mydb = mysql.connector.connect(host="mysql-python", user="root", password="r00tpa55")
+	# c = mydb.cursor()
+	cursor = get_db_connection()
 
 	c.execute("show databases")
 	dbs = [x for x in c]
 
 	if db_name in dbs:
-		return f"Cannot create database {db_name}; it already exists"
+		return f"\nCannot create database {db_name}; it already exists\n"
 	else:
 		c.execute(f"CREATE DATABASE {db_name}")
-		return f"Database {db_name} created. Please confirm by using /get_dbs"
+		return f"\nDatabase {db_name} created. Please confirm by using /get_dbs\n"
 
 
 if __name__ == "__main__":
